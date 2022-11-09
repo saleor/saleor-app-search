47d48c0def3edbe20c61c4714bfef1d7275e5e29
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2022-11-09 10:26:27 +0000

    Add spacing between cards

--- src/components/DashboardActions.tsx
+++ src/components/DashboardActions.tsx
@@ -1,15 +1,22 @@
 import { Card, CardHeader } from "@material-ui/core";
 import { ImportProductsToAlgolia } from "./ImportProductsToAlgolia";
 import AlgoliaConfigurationCard from "./AlgoliaConfigurationCard";
+import { Grid } from "@material-ui/core";
 
 export const DashboardActions = () => {
   return (
     <div>
-      <AlgoliaConfigurationCard />
-      <Card>
-        <CardHeader title="Index actions" />
-        <ImportProductsToAlgolia />
-      </Card>
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <AlgoliaConfigurationCard />
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <CardHeader title="Index actions" />
+            <ImportProductsToAlgolia />
+          </Card>
+        </Grid>
+      </Grid>
     </div>
   );
 };
