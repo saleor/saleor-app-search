19c47ac492f9d5bd1c5a111b549d7af730aea84a
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2022-12-01 19:00:58 +0100

    Handle multiple indexes for search preview + widgets and interface improvements

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,20 +1,29 @@
-import algoliasearch from "algoliasearch/lite";
-import { InstantSearch } from "react-instantsearch-hooks-web";
+import algoliasearch from "algoliasearch";
+import {
+  InstantSearch,
+  Pagination,
+  Breadcrumb,
+  HierarchicalMenu,
+} from "react-instantsearch-hooks-web";
+import { Card, CardContent, CardHeader } from "@material-ui/core";
+import { RangeInput } from "react-instantsearch-hooks-web";
 import "instantsearch.css/themes/reset.css";
 import "instantsearch.css/themes/satellite.css";
 import styles from "../../styles/search.module.css";
 
 import { PageTab, PageTabs } from "@saleor/macaw-ui";
-import { Filters } from "../../components/Filters";
 import { SearchBox } from "../../components/SearchBox";
 import { Hits } from "../../components/Hits";
 import { useRouter } from "next/router";
 import { useAppBridge } from "@saleor/app-sdk/app-bridge";
 import { useConfiguration } from "../../lib/configuration";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+import { Select, MenuItem } from "@material-ui/core";
+import { useQuery } from "react-query";
 
 function Search() {
   const { appBridgeState } = useAppBridge();
+  const [indexName, setIndexName] = useState<string>();
   const algoliaConfiguration = useConfiguration(appBridgeState?.domain, appBridgeState?.token);
 
   const searchClient = useMemo(() => {
@@ -27,10 +36,21 @@ function Search() {
   const router = useRouter();
   const handleClick = (val: string) => router.push("/" + val);
 
-  // todo: Provide utils to get available index names
-  const indexName = algoliaConfiguration?.data?.indexNamePrefix
-    ? `${algoliaConfiguration?.data?.indexNamePrefix}.default-channel.USD.products`
-    : "default-channel.USD.products";
+  const indicesQuery = useQuery({
+    queryKey: ["indices"],
+    queryFn: () => searchClient?.listIndices(),
+    onSuccess: (data) => {
+      // auto select the first fetched index to display its contents
+      if (data?.items?.length) {
+        setIndexName(data.items[0].name);
+      }
+    },
+    enabled: !!searchClient,
+  });
+
+  const availableIndices = indicesQuery.data?.items.map((index) => index.name) || [];
+
+  const displayInterface = !!searchClient && indexName;
 
   return (
     <>
@@ -38,16 +58,55 @@ function Search() {
         <PageTab label={"Configuration"} value="" />
         <PageTab label={"Preview"} value="search" />
       </PageTabs>
-      {!!searchClient && (
+
+      {displayInterface ? (
         <InstantSearch searchClient={searchClient} indexName={indexName}>
           <div className={styles.grid}>
-            <Filters />
+            <div className={styles.filterGrid}>
+              <Card>
+                <CardHeader title={"Index"} />
+                <CardContent>
+                  <Select
+                    labelId="index-select-label"
+                    id="index-select"
+                    value={indexName}
+                    onChange={(event) => setIndexName(event.target.value as string)}
+                  >
+                    {availableIndices.map((name) => (
+                      <MenuItem key={name} value={name}>
+                        {name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </CardContent>
+              </Card>
+
+              <Card>
+                <CardHeader title={"Categories"} />
+                <CardContent>
+                  <HierarchicalMenu
+                    attributes={["categories.lvl0", "categories.lvl1", "categories.lvl1"]}
+                  />
+                </CardContent>
+              </Card>
+
+              <Card>
+                <CardHeader title={"Filters"} />
+                <CardContent>
+                  <RangeInput attribute="grossPrice" />
+                </CardContent>
+              </Card>
+            </div>
             <div className={styles.contentWrapper}>
+              <Breadcrumb attributes={["categories.lvl0", "categories.lvl1", "categories.lvl2"]} />
               <SearchBox />
               <Hits />
+              <Pagination />
             </div>
           </div>
         </InstantSearch>
+      ) : (
+        <p>Loading...</p>
       )}
     </>
   );
